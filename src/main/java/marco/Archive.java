020045c7070546b8f20b6befaf2c2c66e2d5ea1a
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-03-15 14:02:16 +0100

    funzione per autore

--- src/main/java/marco/Archive.java
+++ src/main/java/marco/Archive.java
@@ -29,6 +29,15 @@ public class Archive {
         return foundElement;
     }
 
+    public static List<CatalogElement> searchByYear(int year) {
+
+        return catalogElements.stream().filter(element -> element.getYear() == year).toList();
+
+    }
+
+    public static List<CatalogElement> searchByAuthor(String author) {
+        return catalogElements.stream().filter(element -> element instanceof Book).map(element -> element).filter(element -> ((Book) element).getAuthor().equals(author)).toList();
+    }
 
     public static void main(String[] args) {
 
@@ -42,6 +51,9 @@ public class Archive {
         addElement(magazine1);
         Magazine magazine2 = new Magazine(333333333, "Geographic", 2005, 30, Periodicity.MONTHLY);
         addElement(magazine2);
+        Magazine magazine3 = new Magazine(444444444, "ProvaMagazine", 2000, 150, Periodicity.SEMIANNUAL);
+        addElement(magazine3);
+
         catalogElements.forEach(element -> System.out.println(element));
 
         removeElement(555555555);
@@ -50,7 +62,9 @@ public class Archive {
 
         System.out.println("Elemento trovato " + findElementIsbn(333333333));
 
+        System.out.println("Elementi per anni" + searchByYear(2000));
 
+        System.out.println("Ricerca per autore" + searchByAuthor("Rowling"));
         // -------------------------------
     }
 
